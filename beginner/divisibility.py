7a9150c76a0b675f4aff0dd247964adefea10105
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-11-17 13:06:29 +0100

    chore: the problem of divisibility
    create a program which find all numbers between 1000 and 1500 that
    are divisible by 7 but are not a multiple of 5.

---
+++ beginner/divisibility.py
@@ -0,0 +1,9 @@
+number_list = []
+
+for number in range(1000, 1500):
+    if (number % 7 == 0) and (number % 5 == 0):
+        number_list.append(str(number))
+
+raw_tuple = ', '.join(number_list)
+
+print(raw_tuple)
