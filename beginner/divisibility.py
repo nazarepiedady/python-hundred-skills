485b5be59bd38e6b3355a2f0f3f151bdaedfbf5e
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-11-17 13:09:55 +0100

    fix: second if condition to express difference between (number, 5)

--- beginner/divisibility.py
+++ beginner/divisibility.py
@@ -1,7 +1,7 @@
 number_list = []
 
 for number in range(1000, 1500):
-    if (number % 7 == 0) and (number % 5 == 0):
+    if (number % 7 == 0) and (number % 5 != 0):
         number_list.append(str(number))
 
 raw_tuple = ', '.join(number_list)
