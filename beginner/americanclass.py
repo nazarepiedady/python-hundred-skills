7652655030ee21f2d5cc8df998c855516192a138
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-12-11 09:55:56 +0100

    chore: plays with python class again
    now make a New Yorker class that inherit from American class

---
+++ beginner/americanclass.py
@@ -0,0 +1,13 @@
+class American:
+    pass
+
+
+class New_Yorker(American):
+    pass
+
+
+american_person = American()
+new_yorker_person = New_Yorker()
+
+print(american_person)
+print(new_yorker_person)
